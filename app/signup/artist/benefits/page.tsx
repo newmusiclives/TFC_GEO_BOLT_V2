8d27ddce79fd036533695859f3a43b46b137c49f
Paul Saunders <paul@lightworkdigital.com>
2025-07-31 08:54:27 -0700

    Updated page.tsx

--- app/signup/artist/benefits/page.tsx
+++ app/signup/artist/benefits/page.tsx
@@ -89,7 +89,7 @@ export default function ArtistBenefitsPage() {
           </p>
           <div className="flex gap-4 justify-center">
             <Link href="/signup/artist">
-              <Button className="bg-purple-600 hover:bg-purple-700" size="lg">
+              <Button className="bg-purple-white hover:bg-white/10" size="lg">
                 <Music className="w-5 h-5 mr-2" />
                 Join as an Artist
               </Button>
